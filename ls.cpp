55f8d9ebaa549d7f48874a68d22a37b38ac1c014
David Quintana <gigaherz@gmail.com>
2013-10-31 13:10:01 +0100

    Even better

--- ls.cpp
+++ ls.cpp
@@ -4,46 +4,8 @@
 
 class LsCommand : public Command
 {
-public:
-    LsCommand()
-        : Command(String(_T("ls")), RegisterAsCommand | RegisterAsFunction)
-    {
-    }
-
-    // if used as a command, ls lists the contents of the directory
-    virtual bool ExecCommand(StringVector params, String cmdline)
-    {
-        WIN32_FIND_DATA ffd;
-        String pwd;
-
-        Environment::LoadCurrentDirectory(pwd);
-
-        pwd.append(_T("\\*"));
-        
-        HANDLE hFind = FindFirstFile(pwd.c_str(), &ffd);
-        if (hFind == INVALID_HANDLE_VALUE)
-        {
-            _tcout << _T("ERROR: Could not enumerate directory: ") << GetLastError() << endl;
-            return false;
-        }
-
-        do
-        {
-            _tcout << ffd.cFileName << endl;
-        } while (FindNextFile(hFind, &ffd) != 0);
-
-        DWORD dwError = GetLastError();
-        if (dwError != ERROR_NO_MORE_FILES)
-        {
-            _tcout << _T("ERROR: While enumerating directory: ") << dwError << endl;
-        }
-
-        FindClose(hFind);
-        return true;
-    }
-
-    // if used as a function, ls returns the list as a string
-    virtual bool ExecFunction(String &rettext, StringVector params, String cmdline)
+    template<bool std>
+    bool List(String &rettext)
     {
         WIN32_FIND_DATA ffd;
         String pwd;
@@ -63,12 +25,23 @@ public:
         bool first = true;
         do
         {
-            if (!first)
-                rettext.append(_T(" "));
-            rettext.append(_T("\""));
-            rettext.append(ffd.cFileName);
-            rettext.append(_T("\""));
+            if (std)
+            {
+                _tcout << ffd.cFileName << endl;
+            }
+            else
+            {
+                if (!first)
+                    rettext.append(_T(" "));
+                rettext.append(_T("\""));
+                rettext.append(ffd.cFileName);
+                rettext.append(_T("\""));
+            }
             first = false;
+        } while (FindNextFile(hFind, &ffd) != 0);
+
+        do
+        {
         } while (FindNextFile(hFind, &ffd) != 0);
 
         DWORD dwError = GetLastError();
@@ -79,6 +52,25 @@ public:
 
         FindClose(hFind);
         return true;
+    }
+
+public:
+    LsCommand()
+        : Command(String(_T("ls")), RegisterAsCommand | RegisterAsFunction)
+    {
+    }
+
+    // if used as a command, ls lists the contents of the directory
+    virtual bool ExecCommand(StringVector params, String cmdline)
+    {
+        String dummy;
+        return List<true>(dummy);
+    }
+
+    // if used as a function, ls returns the list as a string
+    virtual bool ExecFunction(String &rettext, StringVector params, String cmdline)
+    {
+        return List<false>(rettext);
     }
 
 } Ls_Instance;
